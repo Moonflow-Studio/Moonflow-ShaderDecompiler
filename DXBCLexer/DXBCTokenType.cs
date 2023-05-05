a0179d534616f2bfd53be6387c0ad4e8e7d87373
liminghao <liminghao120608@pwrd.com>
2023-05-05 20:57:42 +0800

    新增tokentype

--- DXBCLexer/DXBCTokenType.cs
+++ DXBCLexer/DXBCTokenType.cs
@@ -11,15 +11,18 @@ public enum DXBCTokenType
     Comma,				// ,
     
     // 0 cost operators
-    Constant,            // l
-    Absolute,            // abs
+    Constant,           // l
+    Absolute,           // abs
+    Zero,               // _z
+    NotZero,            // _nz
+    Indexable,          // _indexable
     
     //1 line operators
-    Add,				// +
-    And,				// &
-    DerivRtx,			// ddx
-    DerivRty,			// ddy
-    Divide,				// /
+    Add,				// add
+    And,				// and
+    DDX,			    // deriv_rtx
+    DDY,			    // deriv_rty
+    Divide,				// div
     Dot,				// dp2 dp3 dp4
     Exp,				// exp
     Frac,				// frc
@@ -28,36 +31,61 @@ public enum DXBCTokenType
     Max,                // max
     Min,                // min
     Move,				// mov
+    MoveC,				// movc
+    Multiply,			// mul
+    Negate,				// neg
+    Power,				// pow
+    Floor,              // round_ni
+    Round,              // round_ne
+    Ceil,               // round_pi
+    Trunc,              // round_z
+    Rsq,				// rsq
+    Sincos,				// sincos
+    Sqrt,				// sqrt
     
+
     //int operators
     IAdd,				// iadd
-    IMulAdd,				// imad
+    IMulAdd,			// imad
     IMax,				// imax
     IMin,				// imin
     IMul,				// imul
-    INegative,				// ineg
-    IShifLeft,				// ishl
-    IShifRight,				// ishr
+    INegative,			// ineg
+    IShifLeft,			// ishl
+    IShifRight,			// ishr
 
+    //uint operators
+    UDive,				// udiv
+    UMultiAdd,			// umad
+    UMax,				// umax
+    UMin,				// umin
+    UMulti,				// umul
+    UShiftRight,		// ushr
+    
 
     //logic operators
     Break,				// break
-    BreakC,				// breakc
+    BreakCondition,	    // breakc
     Call,				// call
-    CallC,				// callc
+    CallCondition,		// callc
     Case,				// case
     Continue,			// continue
-    ContinueC,			// continuec
+    ContinueCondition,	// continuec
     Default,			// default
     Discard,			// discard
     Else,				// else
     EndIf,				// endif
     EndLoop,			// endloop
     EndSwitch,			// endswitch
-    IEqual,                // ieq
+    IEqual,             // ieq
     If,					// if
     Label,				// label
     Loop,				// loop
+    Not,				// not
+    Or,					// or
+    Ret,				// ret
+    RetC,				// retc
+    Switch,				// switch
     
     
     //Transfer operators
@@ -67,15 +95,54 @@ public enum DXBCTokenType
     UtoF,				// utof
     
     //Comparison operators
+    Equal,				// eq
     GreatEqual,			// ge
     IGreatEqual,		// ige
     ILessThan,			// ilt
     LessThan,			// lt
+    UGreatEqual,		// uge
+    ULessThan,			// ult
+    Xor,				// xor
     
     //sample operators
     Load,				// ld
     LoadFromArray,		// ld2dms
     LOD,				// lod
+    Sample,				// sample
+    Bias,			    // _b
+    Cmp,			    // _c
+    LevelZero,	        // _c_lz
+    Deriv,		        // _d
+    Lod,		        // _l
+    SampleInfo,		    // sampleinfo
+    SamplePos,		    // samplepos
+    
+    //declare part
+    Dcl,				// dcl
+    GlobalFlags,		// _globalFlags
+    ConstantBuffer,		// _constantbuffer
+    ImmediateConstantBuffer,	// _immediateConstantbuffer
+    Input,				// _input
+    Output,				// _output
+    Sampler,			// _sampler
+    Resource,			// _resource
+    Texture2D,          // _texture2d
+    TextureCube,        // _texturecube
+    Buffer,             // _buffer
+    Temps,				// _temps
+    VertexShader,		// _vs
+    PixelShader,		// _ps
+    IndexableTemp,		// _indexableTemp
+    IndexRange,			// _indexRange
+    SV,                 // _sv
+    Depth,              // odepth
+    SIV,                // _siv
+    UAV,                // _uav
+    SGV,                // _sgv
+    OutputTopology,     // OutputTopology
     
     
+    
+
+
 }
