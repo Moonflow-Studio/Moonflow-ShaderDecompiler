52dbbe2744e596fc6fbea284aa08531e9939cc05
Reguluz <154496676@qq.com>
2024-11-26 11:31:39 +0800

    正确导出贴图

--- RenderDocDataExporter/DrawcallExporter.py
+++ RenderDocDataExporter/DrawcallExporter.py
@@ -4,14 +4,12 @@ from pathlib import Path
 filename = "E:\Intime\FrameCapture\AFK\MuMuVMMHeadless_2024.11.13_15.35_frame2730.rdc"
 target_folder = "E:\\Intime\\FrameCapture\\AFK\\Release"
 
-
 # Import renderdoc if not already imported (e.g. in the UI)
 if 'renderdoc' not in sys.modules and '_renderdoc' not in sys.modules:
     import renderdoc
 
 if 'qrenderdoc' not in sys.modules and '_qrenderdoc' not in sys.modules:
     import qrenderdoc
-
 
 # Alias renderdoc for legibility
 rd = renderdoc
@@ -85,7 +83,7 @@ def cbuffer_list_to_text(cblist):
     indent = ''
     varstr = ''
     for sv in cblist:
-        varstr += variable_to_text(sv,'')
+        varstr += variable_to_text(sv, '')
     return varstr
 
 
@@ -96,16 +94,18 @@ def disassemble_cbuffers(controller, refl, state, stage, pipeline, eventId):
     entry = state.GetShaderEntryPoint(stage)
     cbuffer_list = []
     buffer_index = 0
-    stage_name = str(stage).replace("ShaderStage.",'')
+    stage_name = str(stage).replace("ShaderStage.", '')
     while buffer_index < block_count:
-        resource_name = str(blocks[buffer_index].descriptor.resource).replace('ResourceId::','')
+        resource_name = str(blocks[buffer_index].descriptor.resource).replace('ResourceId::', '')
         if buffer_index == 1:
             new_file_name = 'CBuffer_' + stage_name + '_' + str(buffer_index) + '_' + resource_name + '_UnityPerDraw'
         elif buffer_index == 2:
-            new_file_name = 'CBuffer_' + stage_name + '_' + str(buffer_index) + '_' + resource_name + '_UnityPerMaterial'
+            new_file_name = 'CBuffer_' + stage_name + '_' + str(
+                buffer_index) + '_' + resource_name + '_UnityPerMaterial'
         else:
             new_file_name = 'CBuffer_' + stage_name + '_' + str(buffer_index) + '_' + resource_name + '_Unknown'
-        cbuffer_variables = controller.GetCBufferVariableContents(pipeline, refl.resourceId, stage, entry, buffer_index, blocks[buffer_index].descriptor.resource,0,0)
+        cbuffer_variables = controller.GetCBufferVariableContents(pipeline, refl.resourceId, stage, entry, buffer_index,
+                                                                  blocks[buffer_index].descriptor.resource, 0, 0)
         # if len(cbuffer_variables):
         #     new_file_name = 'CBuffer_' + stage_name + '_' + str(buffer_index) + '_' + resource_name + '_UnityPerMaterial'
         # else:
@@ -122,11 +122,14 @@ def disassemble_cbuffers(controller, refl, state, stage, pipeline, eventId):
 def disassemble_textures(ctrl, refl, state, stage, path):
     ress = refl.readOnlyResources
     sress = state.GetReadOnlyResources(stage, True)
+    stage_name = str(stage).replace("ShaderStage.", '')
     if len(ress) == len(sress):
         index = 0
         while index < len(ress):
             if sress[index].descriptor.type == rd.DescriptorType.ImageSampler:
-                resName = 's'+str(ress[index].fixedBindSetOrSpace)+'_b'+str(ress[index].fixedBindNumber)+'_'+str(ress[index].name)+'_'+ str(sress[index].descriptor.resource).replace('ResourceId::','')
+                resName = stage_name+'_s' + str(ress[index].fixedBindSetOrSpace) + '_b' + str(
+                    ress[index].fixedBindNumber) + '_' + str(ress[index].name) + '_' + str(
+                    sress[index].descriptor.resource).replace('ResourceId::', '')
                 texsave = rd.TextureSave()
                 texsave.resourceId = sress[index].descriptor.resource
                 if texsave.resourceId == rd.ResourceId.Null():
@@ -135,8 +138,8 @@ def disassemble_textures(ctrl, refl, state, stage, path):
                 texsave.mip = 0
                 texsave.slice.sliceIndex = 0
                 texsave.destType = rd.FileType.PNG
-                realPath = path+resName
-                ctrl.SaveTexture(texsave, realPath+".png")
+                realPath = path + resName
+                ctrl.SaveTexture(texsave, realPath + ".png")
             index = index + 1
 
 
@@ -151,22 +154,23 @@ def disassemble_vertex_shader(ctrl, state, pipeline, target, evt_id):
     shader_resId = shader_refl.resourceId
 
     disassemble_cbuffers(ctrl, shader_refl, state, rd.ShaderStage.Vertex, pipeline, evt_id)
+    disassemble_textures(ctrl, shader_refl, state, rd.ShaderStage.Vertex, target_folder + '\\' + str(evt_id) + '\\')
 
 
-def disassemble_pixel_shader(ctrl, state, pipeline, target, eventId):
+def disassemble_pixel_shader(ctrl, state, pipeline, target, evt_id):
     shader_refl = state.GetShaderReflection(rd.ShaderStage.Pixel)
     pixel_shader_text = ctrl.DisassembleShader(pipeline, shader_refl, target)
-    text_path = Path(target_folder + '\\' + str(eventId) + '\\' + 'ps.txt')
+    text_path = Path(target_folder + '\\' + str(evt_id) + '\\' + 'ps.txt')
     text_path.write_text(pixel_shader_text)
 
     entryPoint = state.GetShaderEntryPoint(rd.ShaderStage.Pixel)
     shader_resId = shader_refl.resourceId
 
-    disassemble_cbuffers(ctrl, shader_refl, state, rd.ShaderStage.Pixel, pipeline, eventId)
-    disassemble_textures(ctrl, shader_refl, state, rd.ShaderStage.Pixel, target_folder + '\\' + str(eventId) + '\\')
+    disassemble_cbuffers(ctrl, shader_refl, state, rd.ShaderStage.Pixel, pipeline, evt_id)
+    disassemble_textures(ctrl, shader_refl, state, rd.ShaderStage.Pixel, target_folder + '\\' + str(evt_id) + '\\')
 
 
-def disassemble_shader(ctrl, eventId):
+def disassemble_shader(ctrl, evt_id):
     targets = ctrl.GetDisassemblyTargets(True)
     print("Allowed disassembly targets:")
     for t in targets:
@@ -176,12 +180,12 @@ def disassemble_shader(ctrl, eventId):
     state = ctrl.GetPipelineState()
     pipeline = state.GetGraphicsPipelineObject()
 
-    disassemble_vertex_shader(ctrl, state, pipeline, target, eventId)
-    disassemble_pixel_shader(ctrl, state, pipeline, target, eventId)
+    disassemble_vertex_shader(ctrl, state, pipeline, target, evt_id)
+    disassemble_pixel_shader(ctrl, state, pipeline, target, evt_id)
 
 
-def extract_drawcall_data(ctrl, eventId):
-    disassemble_shader(ctrl, eventId)
+def extract_drawcall_data(ctrl, evt_id):
+    disassemble_shader(ctrl, evt_id)
     return
 
 
@@ -194,7 +198,7 @@ def sample_drawcall(drawcall, index):
     print(" ")
     print(" ")
     print("Action #%d Pass #0 starts with %d: %s" % (
-    index, drawcall.eventId, drawcall.GetName(controller.GetStructuredFile())))
+        index, drawcall.eventId, drawcall.GetName(controller.GetStructuredFile())))
 
     if drawcall != None:
         drawcall_path = Path(target_folder + '\\' + str(drawcall.eventId))
